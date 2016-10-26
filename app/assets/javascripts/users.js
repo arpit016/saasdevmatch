2ede4308f732f8b08d04e19d5d46233e0e263543
Arpit Agarwal <agarwalarpit22@gmail.com>
2016-10-26 21:27:45 +0000

    Fixed stripe bad request error

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -5,18 +5,14 @@ $(document).ready(function() {
         event.preventDefault();
         $('input[type=submit]').prop('disabled', true);
         var error = false;
-        var ccNum = $('#card_number').val(),
-            cvcNum = $('card_code').val(),
-            expMonth = $('card_month').val(),
-            expYear = $('card_year').val();
+        // var ccNum = $('#card_number').val(),
+        //     cvcNum = $('card_code').val(),
+        //     expMonth = $('card_month').val(),
+        //     expYear = $('card_year').val();
+        //     console.log(ccNum);
             
         if (!error) {
-            Stripe.createToken({
-                number: ccNum,
-                cvc: cvcNum,
-                exp_month: expMonth,
-                exp_year: expYear
-            }, stripeResponseHandler);
+            Stripe.card.createToken($('#new_user'), stripeResponseHandler);
         }
         return false;
     }); //form submission
@@ -27,6 +23,7 @@ $(document).ready(function() {
         
         //Get the token from the response
         var token = response.id;
+        console.log("Token:" + token);
         
         //Add the token to the form
         f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />');
