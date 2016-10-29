4261ebf88a0558130ed8fa7f40d71b52bf0a5954
Arpit Agarwal <agarwalarpit22@gmail.com>
2016-10-29 19:43:01 +0000

    Fixed typo in form and added redirection if tried to access other user form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,8 +4,14 @@ class ProfilesController < ApplicationController
     def new
         #form for user to fill his profile 
         #No other user should be able to access other user profile to edit or fill his profile
-        @user = User.find(params[:user_id])
-        @profile = @user.build_profile
+        if correct_user
+            @user = User.find(params[:user_id])
+            @profile = @user.build_profile
+        else
+           flash[:warning] = "You cannot fill out other people profile"
+           redirect_to new_user_profile_path(user_id:current_user.id)
+           #render action: :new
+        end
     end
     
     def create
@@ -23,6 +29,14 @@ class ProfilesController < ApplicationController
     private
         def profile_params
            params.require(:profile).permit(:first_name, :last_name, :contact_email, :job_title, :phone_number, :description) 
+        end
+        
+        def correct_user
+           if current_user.id.to_s == params[:user_id]
+               return true
+           else
+               return false
+           end
         end
     
 end
