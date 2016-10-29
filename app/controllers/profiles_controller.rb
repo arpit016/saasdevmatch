4b68d579636eb9a468e08f4162cef6d207c98115
Arpit Agarwal <agarwalarpit22@gmail.com>
2016-10-29 21:08:57 +0000

    Added edit action and home page UI changes

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,12 +1,12 @@
 class ProfilesController < ApplicationController
-    
+    before_action :authenticate_user!
     
     def new
         #form for user to fill his profile 
         #No other user should be able to access other user profile to edit or fill his profile
         if correct_user
             @user = User.find(params[:user_id])
-            @profile = @user.build_profile
+            @profile = Profile.new
         else
            flash[:warning] = "You cannot fill out other people profile"
            redirect_to new_user_profile_path(user_id:current_user.id)
@@ -23,6 +23,22 @@ class ProfilesController < ApplicationController
         else
             flash[:danger] = "Error occured while updating profile. Please try again."
             redirect_to new_user_profile_path(params[:user_id])
+        end
+    end
+    
+    def edit
+     @user = User.find( params[:user_id] )
+     @profile = @user.profile 
+    end
+  
+    def update
+        @user = User.find( params[:user_id] )
+        @profile = @user.profile
+        if @profile.update_attributes(profile_params)
+          flash[:success] = "Profile Updated!"
+          redirect_to user_path( params[:user_id] )
+        else
+          render action: :edit
         end
     end
     
